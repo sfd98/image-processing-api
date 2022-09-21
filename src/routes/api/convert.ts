6292899d67db665cda38de8cbb4e28920ed277c6
sfd98 <sfd.ict@gmail.com>
2022-09-21 07:31:17 +0200

    implemented sharp functionality

--- src/routes/api/convert.ts
+++ src/routes/api/convert.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import {promises as fsPromises} from "fs";
-//import sharp from "sharp";
+import sharp from "sharp";
 
 const convert = express.Router();
 
 convert.get("/", (req, res) => {
   res.send("Convert API");
+  const imageDirectory :string = "images/" + String(req.query.filename) + ".jpg";
+  console.log(imageDirectory);
 
-  
+  sharp(imageDirectory)
+    .resize(Number(req.query.width), Number(req.query.height))
+    .toFile("thumb/thumb_" + String(req.query.filename) + "_" + Number(req.query.width) + "_" + Number(req.query.height) + ".jpg");
 
 });
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import convert from "./api/convert";
+import sharp from "sharp";
 
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  res.send("main api route");
 });
 
 routes.use("/convert", convert);
