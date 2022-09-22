df2bd3f3e95ae4c596a34e1d9e48759865a2c546
sfd98 <sfd.ict@gmail.com>
2022-09-22 11:12:24 +0200

    Fixed test, added new error handlings, linted

--- src/routes/api/convert.ts
+++ src/routes/api/convert.ts
@@ -4,39 +4,59 @@ import fs from "fs";
 
 const convert = express.Router();
 
-convert.get("/", (req, res) =>{ 
-
+convert.get("/", (req, res) => {
+  //Variable decleration. First three initial Params directly taken from url, last three converted params into usable variables in methods.
   const filenameParam = req.query.filename;
   const widthParam = req.query.width;
   const heightParam = req.query.height;
-  const filename :string = String(filenameParam);
-  const width :number = Number(widthParam);
-  const height :number = Number(heightParam);
-    
-  const imageDirectory :string = "images/" + String(filenameParam) + ".jpg";
-    
-  //Error handling. Check in this order: Query String, file exists, filename parameter set, width or height parameter set
-  try {
-    if (!/\?.+/.test(req.url))
-      throw "No query string!"
-    if (fs.existsSync(imageDirectory) == false) 
-      throw "File not found. Check the spelling of the filename or use another one!";
-    if (!filenameParam)
-      throw "No filename!"
-    if (!widthParam || !heightParam)
-      throw "No width or height!";
-    } catch (err) {
-      console.log(err)
-      res.send(err + " Return to /api to check the syntax again.")
-  };
+  const filename = String(filenameParam);
+  const width = Number(widthParam);
+  const height = Number(heightParam);
+
+  //Variable to access the image directory
+  const imageDirectory: string = "images/" + String(filenameParam) + ".jpg";
 
   //Image processing
-  sharp(imageDirectory)
-    .resize(width, height)
-    .toFile("thumb/thumb_" + filename + "_" + width + "_" + height + ".jpg")
-    .catch(err => console.log(err));
-  res.send("Successful, please check the thumb folder.")
-});
+  try {
+    //Error handling. Check error messages to understand the effect
+    if (!/\?.+/.test(req.url)) {
+      res.status(400);
+      throw "No query string!";
+    }
 
+    if (fs.existsSync(imageDirectory) == false) {
+      res.status(400);
+      throw "File not found. Check the spelling of the filename or use another one!";
+    }
+
+    if (!filenameParam) {
+      res.status(400);
+      throw "No filename!";
+    }
+
+    if (!widthParam || !heightParam) {
+      res.status(400);
+      throw "No width or height!";
+    }
+
+    if (width < 0 || height < 0) {
+      res.status(400);
+      throw "Width and height should be > 0.";
+    }
+
+    //Image processing with sharp
+    sharp(imageDirectory)
+      .resize(width, height)
+      .toFile("thumb/thumb_" + filename + "_" + width + "_" + height + ".jpg")
+      .catch((err) => console.log(err));
+
+    res.send("Successful, please check the thumb folder.");
+    res.status(200);
+  } catch (err) {
+    console.log(err);
+    res.send(err + " Return to /api to check the syntax again.");
+    res.status(400);
+  }
+});
 
 export default convert;
